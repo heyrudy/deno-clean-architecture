5d87627f61178f0e6590849ffaba8dc90a8aed09
Turing00 <kokou.koumagnagottoh@gmail.com>
2020-05-23 17:26:58 +0200

    🏆 http server dependencies

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export { Application, Router } from "https://deno.land/x/oak/mod.ts";
+export { config } from "https://deno.land/x/dotenv/mod.ts";
+export { v4 } from "https://deno.land/std/uuid/mod.ts";
